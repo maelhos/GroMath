628d909438541c980c6f49a6bd645223ec5648a6
maelhos <52194580+maelhos@users.noreply.github.com>
2024-04-11 00:07:26 +0200

    corrected

--- GrosMathFromScratch/build.sh
+++ GrosMathFromScratch/build.sh
@@ -1,7 +1,7 @@
 
 ~/Desktop/Big_Projects/syntax/bin/syntax -g src/parser/GroMathGrammar.yacc -m LALR1 -o src/parser/GroMathParser.h
 
-clang++ -o GroMath -g `llvm-config --cxxflags --ldflags --system-libs --libs core | sed "s/\-fno\-exceptions//"` src/GrosMathLLVM.cpp
+clang++ -fuse-ld=lld -I src/ -o GroMath -g `llvm-config --cxxflags --ldflags --system-libs --libs core | sed "s/\-fno\-exceptions//"` src/GrosMathLLVM.cpp
 
 ./GroMath
 
