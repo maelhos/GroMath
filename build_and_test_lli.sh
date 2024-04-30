01fa5bc16df694689686c55ed5b33d5eca3fba90
maelhos <52194580+maelhos@users.noreply.github.com>
2024-04-30 10:52:46 +0200

    small patch to test build script

--- build_and_test_lli.sh
+++ build_and_test_lli.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-FILES="src/codeGen.cpp src/parser.cpp src/tokens.cpp src/main.cpp"
-bison -d -o src/parser.cpp parser.yacc -t -v
-mv src/parser.hpp include/parser.h
-mv src/parser.output debug/
-lex -o src/tokens.cpp tokens.l
-clang++ -fuse-ld=lld -I include/ -g `llvm-config --cxxflags --ldflags --system-libs --libs core` -DYYDEBUG -o GroMath $FILES
+sh build.sh
 
 ./GroMath test_llvm.gm
 lli test_llvm.gm.ll
